f13419f3580c2d9fa62d0a9d40dcb718d6516df0
Benoit Hamelin <benoit@benoithamelin.com>
2020-02-28 11:49:22 -0500

    Overhaul main Makefile to include a local config
    This local configuration should include the definition of the
    `dask-workers` variable, failing which the system will default to a
    4-thread unique-process Dask worker.

--- dask-util.mk
+++
@@ -1,2 +0,0 @@
-# Utility files to help with configuring worker sets.
-dask-work = $(addsuffix @$(2),$(shell seq --format='%02.f' 1 $(1)))
--- dask.mk
+++ dask.mk
@@ -1,18 +1,30 @@
+# Load local worker configuration.
+dask-n = $(addsuffix @$(2),$(shell seq --format='%02.f' 1 $(1)))
+-include $(HOME)/.dask/local.mk
+ifeq ($(dask-workers),)
+	$(warning No Dask worker defined -- variable \033[1;37mdask-workers\033[0m undefined.)
+	$(warning Defaulting to 4 worker threads in a single local process.)
+	dask-threads-per-worker = 4
+	dask-workers = default@
+endif
+
 # Relevant files for the cluster state and logs.
-pid-file = $(addsuffix .pid,$(1))
-log-file = $(addsuffix .log,$(1))
+in-dask-dir = $(addprefix $(HOME)/.dask/,$(1))
+dask-pid-file = $(call in-dask-dir,$(addsuffix .pid,$(1)))
+dask-log-file = $(call in-dask-dir,$(addsuffix .log,$(1)))
 
 
 # Running daemons in the background as recipe for a target, some transient
 # crumb being used as target.
 define dask-run
 (\
-	rm -f $(call pid-file,$(1));\
-	date "+*** Starting $(strip $(1)) at %Y-%m-%d %H:%M:%S ***" >>$(call log-file,$(1));\
-	$(2) >>$(call log-file,$(1)) 2>&1 &\
-	echo $$! >$(call pid-file,$(1));\
+	mkdir -p $$HOME/.dask;\
+	rm -f $(call dask-pid-file,$(1));\
+	date "+*** Starting $(strip $(1)) at %Y-%m-%d %H:%M:%S ***" >>$(call dask-log-file,$(1));\
+	$(2) >>$(call dask-log-file,$(1)) 2>&1 &\
+	echo $$! >$(call dask-pid-file,$(1));\
 	wait;\
-	rm -f $(call pid-file,$(1))\
+	rm -f $(call dask-pid-file,$(1))\
 ) &
 endef
 
@@ -24,7 +36,7 @@ dask-scheduler-address = $(dask-scheduler-host):$(dask-scheduler-port)
 # Worker default parameters
 dask-worker-port-base = 18700
 dask-workers-sanitized = $(strip $(dask-workers))
-dask-threads-per-worker = 1
+dask-threads-per-worker ?= 1
 
 dask-spawn = scheduler $(addprefix worker-,$(dask-workers-sanitized))
 
@@ -40,23 +52,23 @@ dask-local = $(2)
 dask: dask-scheduler dask-workers
 
 .PHONY: dask-scheduler
-dask-scheduler: $(call pid-file,scheduler)
+dask-scheduler: $(call dask-pid-file,scheduler)
 
-$(call pid-file,scheduler):
+$(call dask-pid-file,scheduler):
 	$(call dask-run,scheduler,dask-scheduler --host $(dask-scheduler-host) --port $(dask-scheduler-port))
 	sleep 2  # Make sure schduler is online before running workers.
 
 .PHONY: dask-workers
-dask-workers: $(call pid-file,$(addprefix worker-,$(dask-workers-sanitized)))
+dask-workers: $(call dask-pid-file,$(addprefix worker-,$(dask-workers-sanitized)))
 
 .PHONY: dask-worker-%
-dask-worker-%: $(call pid-file,worker-%)
+dask-worker-%: $(call dask-pid-file,worker-%)
 
-$(call pid-file,worker-%): dask-name-host = $(subst @, ,$*)
-$(call pid-file,worker-%): dask-name = $(word 1,$(dask-name-host))
-$(call pid-file,worker-%): dask-host = $(word 2,$(dask-name-host))
-$(call pid-file,worker-%): dask-port = $(shell echo $(dask-workers-sanitized) | sed 's/ /\n/g' | awk '($$1 == "$*"){print $(dask-worker-port-base) + NR; exit}')
-$(call pid-file,worker-%): | $(call pid-file,scheduler)
+$(call dask-pid-file,worker-%): dask-name-host = $(subst @, ,$*)
+$(call dask-pid-file,worker-%): dask-name = $(word 1,$(dask-name-host))
+$(call dask-pid-file,worker-%): dask-host = $(word 2,$(dask-name-host))
+$(call dask-pid-file,worker-%): dask-port = $(shell echo $(dask-workers-sanitized) | sed 's/ /\n/g' | awk '($$1 == "$*"){print $(dask-worker-port-base) + NR; exit}')
+$(call dask-pid-file,worker-%): | $(call dask-pid-file,scheduler)
 	$(call dask-run,\
 		worker-$*,\
 		$(call $(if $(dask-host),dask-ssh,dask-local),\
@@ -71,7 +83,7 @@ dask-kill: dask-kill-workers dask-kill-scheduler
 
 .PHONY: dask-kill-%
 dask-kill-%:
-	$(if $(wildcard $(call pid-file,$*)),kill $$(cat $(call pid-file,$*)); sleep 2,@true)
+	$(if $(wildcard $(call dask-pid-file,$*)),kill $$(cat $(call dask-pid-file,$*)),@true)
 
 .PHONY: dask-kill-workers
 dask-kill-workers: $(addprefix dask-kill-worker-,$(dask-workers-sanitized))
@@ -83,18 +95,22 @@ dask-suggest-load:
 	@awk 'BEGIN{n=0} /^processor[ \t]+: [0-9]+/{n+=1} END{print n+0}' /proc/cpuinfo
 
 
+dask-status-file = $(call in-dask-dir,$(addprefix status-,$(1)))
+dask-status-main = $(call in-dask-dir,status)
+
 # Tool: check status of various spawned daemons.
 .PHONY: dask-status
-dask-status: .dask-status
-	@awk -F, '{printf("%s -- %s\n", $$1,$$2 ? "up" : "down")}' .dask-status
+dask-status: $(dask-status-main)
+	@awk -F, '{printf("%s -- %s\n", $$1,$$2 ? "up" : "down")}' $<
 
-.PHONY: .dask-status
-.dask-status: $(addprefix .dask-status-,$(dask-spawn))
+# This is not really a phony target, we just insist on remaking this file every time.
+.PHONY: $(dask-status-main)
+$(dask-status-main): $(call dask-status-file,$(dask-spawn))
 	@cat $+ >$@
 	@rm -f $+
 
-.dask-status-%:
-	@(echo -n $*, ; cat $(call pid-file,$*) 2>/dev/null || echo) >$@
+$(call dask-status-file,%):
+	@(echo -n $*, ; cat $(call dask-pid-file,$*) 2>/dev/null || echo) >$@
 
 
 # Tools for checking and tracking logs.
@@ -102,15 +118,15 @@ dask-status: .dask-status
 dask-check: dask-tail-logs
 
 .PHONY: dask-track
-dask-track: dask-tail-args = -f
+dask-track: dask-tail-args += -f
 dask-track: dask-tail-logs
 
 .PHONY: dask-tail-logs
-dask-tail-logs: .dask-status
-	tail $(dask-tail-args) $(call log-file,$(or $(dask-logs),$(shell awk -F, '{if($$2) print $$1}' .dask-status)))
+dask-tail-logs: $(dask-status-main)
+	tail $(dask-tail-args) $(call dask-log-file,$(or $(dask-logs),$(shell awk -F, '{if($$2) print $$1}' $<)))
 
 
 # Clean-up
 .PHONY: dask-clean
 dask-clean: dask-kill
-	rm -f $(call pid-file,$(dask-spawn)) $(call log-file,$(dask-spawn)) .dask-status*
+	rm -f $(call dask-pid-file,$(dask-spawn)) $(call dask-log-file,$(dask-spawn)) $(dask-status-main) $(call dask-status-file,$(dask-spawn))
